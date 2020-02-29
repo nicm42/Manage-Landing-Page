85948297a21a800c19646c5a1126b17dfae4a28c
Nic Mayer <nicmayer42@gmail.com>
2020-02-29 19:36:58 +0000

    Done mobile navigation

--- script.js
+++ script.js
@@ -2,12 +2,30 @@
 TODO:
 	- nav
 	- testimonials slider
-	- updates email validation (see active-states.jpg)
 */
 
 
-/* validate email input 
-From: https://daverupert.com/2017/11/happier-html5-forms/
+/* 
+	Mobile nav 
+	From: https://www.youtube.com/watch?v=DZg6UfS5zYg
+*/
+const hamburger = document.querySelector('.header__hamburger');
+const menu = document.querySelector('.nav__menu');
+hamburger.addEventListener(
+	'click',
+	event => {
+		menu.classList.toggle('show-menu');
+		if(menu.classList.contains('show-menu')){
+			hamburger.src = "images/icon-close.svg";
+		}else{
+			hamburger.src = "images/icon-hamburger.svg";
+		}
+	},false
+);
+
+/*
+	Validate email input 
+	From: https://daverupert.com/2017/11/happier-html5-forms/
 */
 const email = document.querySelector('.footer__input');
 const invalid = document.querySelector('.footer__invalid');
