3385a1c7cf50d5d44d75104ebbf2a0e3997780da
Nic Mayer <nicmayer42@gmail.com>
2020-03-07 17:27:52 +0000

    Added Gulp to make scss and js files IE compatible

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
+const babel = require('gulp-babel');
+
+
+// File paths
+const files = { 
+    scssPath: 'arc/*.scss',
+    jsPath: 'src/*.js'
+}
+
+// Sass task: compiles the style.scss file into style.css
+function scssTask(){    
+    return src(files.scssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('./')
+    ); // put final CSS in dist folder
+}
+
+// JS task: concatenates and uglifies JS files to script.js
+function jsTask(){
+    return src([
+        files.jsPath
+        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+        ])
+        .pipe(babel())
+        .pipe(dest('./')
+    );
+}
+
+// Cachebust
+function cacheBustTask(){
+    var cbString = new Date().getTime();
+    return src(['index.html'])
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+        .pipe(dest('.'));
+}
+
+// Watch task: watch SCSS and JS files for changes
+// If any change, run scss and js tasks simultaneously
+function watchTask(){
+    watch([files.scssPath, files.jsPath],
+        {interval: 1000, usePolling: true}, //Makes docker work
+        series(
+            parallel(scssTask, jsTask)
+        )
+    );    
+}
+
+// Export the default Gulp task so it can be run
+// Runs the scss and js tasks simultaneously
+// then runs cacheBust, then watch task
+exports.default = series(
+    parallel(scssTask, jsTask), 
+    watchTask
+);
--- script.js
+++ script.js
@@ -4,46 +4,43 @@
 	Mobile nav 
 	From: https://www.youtube.com/watch?v=DZg6UfS5zYg
 */
-var hamburger = document.querySelector(".header__hamburger");
-var menu = document.querySelector(".nav__menu");
-hamburger.addEventListener(
-  "click",
-  function(event) {
-    menu.classList.toggle("show-menu");
+var hamburger = document.querySelector('.header__hamburger');
+var menu = document.querySelector('.nav__menu');
+hamburger.addEventListener('click', function (event) {
+  menu.classList.toggle('show-menu');
 
-    if (menu.classList.contains("show-menu")) {
-      hamburger.src = "images/icon-close.svg";
-    } else {
-      hamburger.src = "images/icon-hamburger.svg";
-    }
-  },
-  false
-);
+  if (menu.classList.contains('show-menu')) {
+    hamburger.src = "images/icon-close.svg";
+  } else {
+    hamburger.src = "images/icon-hamburger.svg";
+  }
+}, false);
 /*
 	Testimonials slider
 */
 
-var slider = document.querySelector(".testimonials__slider");
-var people = document.getElementsByClassName("testimonials__person");
-var dots = document.getElementsByClassName("testimonials__dot"); //Use intersection observer to check if a person is mostly in view
+var slider = document.querySelector('.testimonials__slider');
+var people = document.getElementsByClassName('testimonials__person');
+var dots = document.getElementsByClassName('testimonials__dot'); //Use intersection observer to check if a person is mostly in view
 //If they are, then make that dot filled
 //If not, make that dot empty
 //Only applicable to mobile, as dots not visible on desktop
 
 function onChange(changes, observer) {
-  changes.forEach(function(change) {
+  changes.forEach(function (change) {
     var personId = change.target.id.substring(change.target.id.length - 1);
 
     if (change.intersectionRatio > 0.5) {
-      dots[Number(personId) - 1].classList.add("filled-dot");
+      dots[Number(personId) - 1].classList.add('filled-dot');
     } else {
-      dots[Number(personId) - 1].classList.remove("filled-dot");
+      dots[Number(personId) - 1].classList.remove('filled-dot');
     }
   });
 }
 
 var options = {
   threshold: 0.5 //update when half of the person is visible
+
 };
 var observer = new IntersectionObserver(onChange, options);
 var _iteratorNormalCompletion = true;
@@ -51,22 +48,19 @@ var _didIteratorError = false;
 var _iteratorError = undefined;
 
 try {
-  for (
-    var _iterator = people[Symbol.iterator](), _step;
-    !(_iteratorNormalCompletion = (_step = _iterator.next()).done);
-    _iteratorNormalCompletion = true
-  ) {
+  for (var _iterator = people[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
     var person = _step.value;
     observer.observe(person);
   } //Scroll testimonials section when it's grabbed and slid (ie mousedown then move)
   //https://courses.wesbos.com/account/access/5ca4d00285f96c03c1e3d972/view/194158963
+
 } catch (err) {
   _didIteratorError = true;
   _iteratorError = err;
 } finally {
   try {
-    if (!_iteratorNormalCompletion && _iterator.return != null) {
-      _iterator.return();
+    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+      _iterator["return"]();
     }
   } finally {
     if (_didIteratorError) {
@@ -78,21 +72,21 @@ try {
 var mouseDown = false;
 var startX;
 var scrollLeft;
-slider.addEventListener("mousedown", function(event) {
+slider.addEventListener('mousedown', function (event) {
   mouseDown = true;
-  slider.classList.add("active");
+  slider.classList.add('active');
   startX = event.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
-slider.addEventListener("mouseleave", function() {
+slider.addEventListener('mouseleave', function () {
   mouseDown = false;
-  slider.classList.remove("active");
+  slider.classList.remove('active');
 });
-slider.addEventListener("mouseup", function() {
+slider.addEventListener('mouseup', function () {
   mouseDown = false;
-  slider.classList.remove("active");
+  slider.classList.remove('active');
 });
-slider.addEventListener("mousemove", function(event) {
+slider.addEventListener('mousemove', function (event) {
   if (!mouseDown) {
     return;
   }
@@ -107,13 +101,9 @@ slider.addEventListener("mousemove", function(event) {
 	From: https://daverupert.com/2017/11/happier-html5-forms/
 */
 
-var email = document.querySelector(".footer__input");
-var invalid = document.querySelector(".footer__invalid");
-email.addEventListener(
-  "invalid",
-  function(event) {
-    email.classList.add("error");
-    invalid.style.visibility = "visible";
-  },
-  false
-);
+var email = document.querySelector('.footer__input');
+var invalid = document.querySelector('.footer__invalid');
+email.addEventListener('invalid', function (event) {
+  email.classList.add('error');
+  invalid.style.visibility = "visible";
+}, false);
